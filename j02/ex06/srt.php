c94badd1524b30c0b10d59369f46a3701c5d7447
Thomas GUILLEMARD <tguillem@student.42.fr>
2016-10-05 15:31:09 +0200

    Finish ex06 of day 02

--- j02/ex06/srt.php
+++ j02/ex06/srt.php
@@ -13,24 +13,56 @@ function is_time($a)
 if ($argc != 2 || !file_exists($argv[1]))
     exit();
 
+// save all file lines in a row array
 $buffer = fopen($argv[1], 'r');
 while ($buffer && !feof($buffer))
     $array[] = fgets($buffer);
 
-foreach($array as $key => $value)
-    if (is_time($value))
-        $time[$key] = $value;
 
-sort($time);
+// intialize vars
 $index = 0;
+$time = array();
+$data = array();
+$key_id = 0;
 
-foreach($array as $value)
+foreach($array as $key => $value)
 {
-    if (is_time($value))
+    // Index number
+    if ($index == 0)
     {
-        echo $time[$index];
-        $index++;
-    } else
-        echo $value;
+        $key_id = $value;
+        var_dump($array);
+    }
+    // Time string
+    else if (is_time($value))
+    {
+        $time[intval($key_id)] = $value;
+        $index = 1;
+    }
+    // Additional string
+    else if ($index == 2)
+        $data[$time[intval($key_id)]] = $value;
+    // End of a section, reset index
+    else if ($index == 3)
+        $index = -1;
+    $index++;
+}
+
+// Sort all entries by the date str
+ksort($data);
+sort($time);
+
+// Destroy $array, we don't need it anymore
+unset($array);
+
+// Get the last element of the $time array
+$last = end(array_unique($time));
+
+// Print the sorted response entries
+foreach($time as $key => $time)
+{
+    echo $key . "\n" . $time  . $data[$time];
+    if ($time !== $last)
+        echo "\n";
 }
 ?>
