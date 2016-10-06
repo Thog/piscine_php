e1d65af6c91d9ca362e54a89ecb4ea7883653f74
Thomas GUILLEMARD <tguillem@student.42.fr>
2016-10-06 14:49:01 +0200

    Fix data type of ex06 of day 03

--- j03/ex06/members_only.php
+++ j03/ex06/members_only.php
@@ -7,6 +7,6 @@ if (($_SERVER['PHP_AUTH_USER'] != "zaz") || ($_SERVER['PHP_AUTH_PW'] != "jaimele
 } else
 {
     $file = file_get_contents('../img/42.png');
-    echo "<html><body>\nBonjour Zaz<br />\n<img src='data:image/jpeg;base64," . base64_encode($file) . "'>\n</body></html>\n";
+    echo "<html><body>\nBonjour Zaz<br />\n<img src='data:image/png;base64," . base64_encode($file) . "'>\n</body></html>\n";
 }
 ?>
