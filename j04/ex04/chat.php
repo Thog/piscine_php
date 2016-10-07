d9d1836a8328ed7933a9ddc6521f897d241ebac7
Thomas GUILLEMARD <tguillem@student.42.fr>
2016-10-07 17:47:20 +0200

    Fix line return of ex04/chat.php (D04)

--- j04/ex04/chat.php
+++ j04/ex04/chat.php
@@ -6,6 +6,6 @@ else if (file_exists('../private') && file_exists('../private/chat'))
 {
     $chat = unserialize(file_get_contents('../private/chat'));
     foreach ($chat as $v)
-        echo "[" . date('H:i', $v['time']) . "] <b>" . $v['login'] . "</b>: " . $v['msg'] . "<br />";
+        echo "[" . date('H:i', $v['time']) . "] <b>" . $v['login'] . "</b>: " . $v['msg'] . "<br />\n";
 }
 ?>
